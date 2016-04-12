d574027fa93aff985ec1f5b2f4d0b5ffd8461035
Caleb Washburn <cwashburn@pivotal.io>
2016-04-12 10:19:43 -0500

    fix issue with redis.conf being owned by root and messaging appropriately

--- scripts/backupDedicated.sh
+++ scripts/backupDedicated.sh
@@ -1,12 +1,18 @@
 FILE="/var/vcap/store/redis"
-redis_command=$(awk '/rename-command BGSAVE/{print $3}' $FILE/redis.conf)
-redis_port=$(awk '/port/{print $2}' $FILE/redis.conf)
-redis_pwd=$(awk '/requirepass/{print $2}' $FILE/redis.conf)
-last_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd LASTSAVE)
-bg_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd ${redis_command//\"/})
-new_last_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd LASTSAVE)
-until [ $new_last_save -gt $last_save ]
-do
+if [ -w "$FILE/redis.conf" ]
+then
+  redis_command=$(awk '/rename-command BGSAVE/{print $3}' $FILE/redis.conf)
+  redis_port=$(awk '/port/{print $2}' $FILE/redis.conf)
+  redis_pwd=$(awk '/requirepass/{print $2}' $FILE/redis.conf)
+  last_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd LASTSAVE)
+  bg_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd ${redis_command//\"/})
   new_last_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd LASTSAVE)
-done
-cd /var/vcap/store/ && tar cz redis
+  until [ $new_last_save -gt $last_save ]
+  do
+    new_last_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd LASTSAVE)
+  done
+  cd /var/vcap/store/ && tar cz redis
+else
+  echo "$FILE/redis.conf is not writable" 1>&2
+	exit 1
+fi
--- scripts/backupShared.sh
+++ scripts/backupShared.sh
@@ -1,15 +1,21 @@
 for FILE in /var/vcap/store/cf-redis-broker/redis-data/*; do
     if [[ -d $FILE ]]; then
-       redis_command=$(awk '/rename-command BGSAVE/{print $3}' $FILE/redis.conf)
-       redis_port=$(awk '/port/{print $2}' $FILE/redis.conf)
-       redis_pwd=$(awk '/requirepass/{print $2}' $FILE/redis.conf)
-       last_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd LASTSAVE)
-       bg_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd ${redis_command//\"/})
-       new_last_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd LASTSAVE)
-       until [ $new_last_save -gt $last_save ]
-       do
-        new_last_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd LASTSAVE)
-       done
+       if [ -w "$FILE/redis.conf" ]
+       then
+         redis_command=$(awk '/rename-command BGSAVE/{print $3}' $FILE/redis.conf)
+         redis_port=$(awk '/port/{print $2}' $FILE/redis.conf)
+         redis_pwd=$(awk '/requirepass/{print $2}' $FILE/redis.conf)
+         last_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd LASTSAVE)
+         bg_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd ${redis_command//\"/})
+         new_last_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd LASTSAVE)
+         until [ $new_last_save -gt $last_save ]
+         do
+          new_last_save=$(/var/vcap/packages/redis/bin/redis-cli -h 127.0.0.1 -p $redis_port -a $redis_pwd LASTSAVE)
+         done
+      else
+        echo "$FILE/redis.conf is not writable" 1>&2
+        exit 1
+      fi
     fi
 done
 cd /var/vcap/store/cf-redis-broker/ && tar cz redis-data
